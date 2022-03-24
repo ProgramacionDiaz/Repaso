9d9806aae7d5fc08bf32b3f83064ee0e7650c75b
ProgramacionDiaz <101899151+ProgramacionDiaz@users.noreply.github.com>
2022-03-24 17:29:43 -0500

    Add files via upload
    24032022

---
+++ usminia11/actividad.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <h1> Hola soy un titulo</h1>
+            <?php
+            //INICIA CODIGO PHP
+            //ESTO ES UN COMENTARIO
+            /*Todo lo que este aqui adentro se 
+            considera un comentario
+            */
+            //CUANDO ECHO DEVUELVE UN VALOR
+            echo "Hola soy un parrafo";
+            echo "<br>";  //Salto de linea 
+            echo 3+3;    /*Realiza operaciones en php no se puede en html.*/
+            //TERMINA PHP
+            ?>
+    </body>
+</html>
